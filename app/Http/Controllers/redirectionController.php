4e27de62478306e5b87d2cbc1fb4ce280d795c25
TopeNii1221 <kristoferentierro24@gmail.com>
2024-05-22 01:28:02 +0800

    Added Terms, Privacy, Contact, and About page.

--- app/Http/Controllers/redirectionController.php
+++ app/Http/Controllers/redirectionController.php
@@ -6,7 +6,26 @@ use Illuminate\Http\Request;
 
 class redirectionController extends Controller
 {
-    
+    public function about(){
+
+    	return view('about-us');
+
+    }
+    public function contact(){
+
+    	return view('contact-us');
+
+    }
+    public function terms(){
+
+    	return view('terms');
+
+    }
+    public function privacy(){
+
+    	return view('privacy');
+
+    }
     public function index(){
 
     	return view('index');
